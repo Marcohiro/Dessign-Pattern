383da54ac9882aa777b5927e2702edc0c12d4da3
Marcohiro <petit.marcalain@yahoo.fr>
2020-03-24 08:42:30 +0100

    added classes

---
+++ DesignPattern/Ingredients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    class Ingredients
+    {
+        public bool isVegetarian { get;  }
+        public string name { get; }
+
+        public Ingredients(string name, bool isVegetarian)
+        {
+            this.name = name;
+            this.isVegetarian = isVegetarian;
+        }
+
+        public bool isIngredientVegetarian()
+        {
+            return this.isVegetarian;
+        }
+    }
+}
---
+++ DesignPattern/Kebab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern
+{
+    class Kebab
+    {
+
+        public List<Ingredients> ingredients { get; }
+
+        bool isVegetarian()
+        {
+            foreach(Ingredients ingredient in ingredients)
+            {
+                if (!ingredient.isVegetarian) return false;
+            }
+            return true;
+        }
+    }
+}
